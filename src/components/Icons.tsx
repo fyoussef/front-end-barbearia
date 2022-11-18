355f3d20c3438ae391ab18269ed71e8a11f3ed06
fyoussef <fillipyuou@gmail.com>
2022-11-18 14:37:05 -0300

    Criando layout para exibição dos horários disponíveis

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -21,3 +21,26 @@ export function MustacheIcon() {
         
     )    
 }
+
+export function AvatarIcon() {
+    return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="64"
+          height="64"
+          x="0"
+          y="0"
+          version="1.1"
+          viewBox="0 0 64 64"
+          xmlSpace="preserve"
+        >
+          <image
+            width="64"
+            height="64"
+            x="0"
+            y="0"
+            href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAQAAAAAYLlVAAAABGdBTUEAALGPC/xhBQAAACBjSFJN AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZ cwAAAdgAAAHYAfpcpnIAAAAHdElNRQfmCxISKB9O/ih7AAAD9UlEQVRo3r2ZX2zTVRTHP/25LoK1 bBO2SIISmdMxFf9tfbIPijEmw2iCQV6mQ1Gib4YHQggiiQ8GkESeIJmEaAbBEI0JJCQEEgsGGAsE oVnQCYHh2BhbNyCkDvrzoXTt2t57z/39fuXcp+aecz7f3HP//W5D2Fo9cVp4liZqqSFCFSkyDHOR fpIcJUnGOqfQWvmOP8ngatso+1jGzGDRUVaTNICnt1v8wHPBwOv4mlEreK5lOECbP3iIDoY9wfMi 9jLPK76RY77guTZOhxf8e4wFgs+2n6m1gTtsDRCebUnmS/HVdAeOd3EZ5BUZfn9F8NnZYJQQYlfF 8C4u11moFxB87YvbZRrU+PcrjndxOcxD5fGNjIuT/MduPqCZ2cwlxlrOW0nYWL768m3nIE+XLNxO C/l3eblUwMfi8G2KIWzhsjjHSZzpwXVcF4Z2E1LOoRe4KZbw2fTQjcKwq4aT/guL1VCdD4uKD9xV hn0kzD9iCZ/mw1YLQyapw2TfigX8lS+m9LbTY8TDW2IBLnFwgFaaBYkBrgbkk7MPswKWiwMmBT5p CwFLCTvAm+KAxwU+9RYCorQ51NMiDljEw0afVgsBsNghrtlYii3C60afd6wExGGDxax1OW6QGzN+ uExvg1hfvz7R4Gdy2jKbC6csA9K8ocCH2WeNd+GSdUiaz4vPMqCBwx7wLox4CutlGY9OwRvZwISn PG6IdOGppLVxDtDDbdpZAkCafoao4gmevO/xG/t5hDbeZpZ8IaRFSjNsLkjazpUSjyu0T/XPYrN4 NYhKkKGzSPYMVnJmqv80K5lR5NEplCCahN8rRq+eGDHmKHq3yQSYl2GKqLyiBRYlZRbgcMGYqIsJ TwIm6JK4fWVU2eQJD9BkzD3icM6QZEAwRiq7wIDBo9chgat1SXjGS6ITDsOGMUj6EmCKTjjAIa3L RV8C9NE3+MMBurVOg74E6KN/YdIBTmkH6o4vAfroPdw/VndqnO76EqCL7uNITsB2xpRuj/kSMEfT t4VMTsBN5W5ve8stNvVT7b/8WPizVvkk20eVZ3yYv5V74EfFzp1K17WeBai3+ROll7oQRxXO91hj 8e2Qz7dGeSOYzBe2MPECepVXqbNsJ8E10ZoI08BrrOJ5pcc6vinf8WCe6Y6UuVNP2ZaK4/v1n68h dlYUP8Iz5gpW7rF6lJhk/lbRVRH8NV6U4LOF2BQ4PskCKT5r73r8r6x8+4mIHR7gKX4PqPIr7OG5 UnQw5BO/1+rVqIzVsJ4bntAZfuVVf/CcRfiSc1bwFDtYFAw8by+xiTPcM6CH2M3ykk9VTZVtbTZx FtJME3XUEKGa26QYY4Dz9NHDWVybdP8DvnZcqB45C10AAAAldEVYdGRhdGU6Y3JlYXRlADIwMjIt MTEtMThUMTc6NDA6MzErMDE6MDCkHb/+AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIyLTExLTE4VDE3 OjQwOjMxKzAxOjAw1UAHQgAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAA SUVORK5CYII="
+          ></image>
+        </svg>
+    );
+}
