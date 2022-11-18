a4b1c063eba9fc860a22060f2fd4d5b96641e685
fyoussef <fillipyuou@gmail.com>
2022-11-18 00:58:25 -0300

    Primeiro commit

---
+++ src/components/Icons.tsx
@@ -0,0 +1,23 @@
+export function MustacheIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="64"
+            height="64"
+            x="0"
+            y="0"
+            version="1.1"
+            viewBox="0 0 64 64"
+            xmlSpace="preserve"
+        >
+            <image
+            width="64"
+            height="64"
+            x="0"
+            y="0"
+            href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAQAAAAAYLlVAAAABGdBTUEAALGPC/xhBQAAACBjSFJN AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZ cwAAAkoAAAJKAdorXWYAAAAHdElNRQfmCxIEDy1ERxRcAAACM0lEQVRo3u2Yz0sUYRjHP+/Mbm27 0C+TMSkWCpNADxF0ETx4jgg6urcuElLQHxCE5zrURSLIU0j/gF5EEApFTERKUragZXfBDI1I2WaH p8NsOeZsMzu7Ngjv+5zm/c7zfD8w7/u8zKuEeIcRs78G0AAaQANoAA2gATSABjiEAKpN9aqUr5JS PepUwwQSMjjJMG/YRBBsFhglhykImOQYZQEbQdhgmhxHQ9cN9ZLFc3Z8hFm66WbWR1nnLqolABjc YauuvM12XW2Ss00DcJX50F9pf3zhZiAAF7jHMV/pOE9xmrB34xnpOtYnuM9lmECYdJfTHvk6habN 3XhPr499G4sI07CKIDzeI7bzskXmbuww9Jf9OZYQhE+wUZt7QlYQOM8jvrfU3o13DJERBDIM8602 u6n4SbLWEhxW6OT0Aba9LZYRrrHbyGxFkc4DtAwaRYNCjPZQMPgYK0De4FWsAOOKBAU6IheoAonI 2UWyhlQZi1ygzAADlCPnvxDHbblvI+3sKSxBwGIqUv4cmdphhEW+weQKI7vtG5MRKg1W+MAZ4c9p yEVWQqeu85COfb3d4gGl0DWW3L7rOY4xuc3ngLSvjDNIqp5MkluMUQ6okmcQ4/eD8l5UqhQ36Kef HpRnusQaa6zymjlxgleWUlyhj0t00UUW07NjFplhhgmxPW/73ZSqNGmSJEhiUpIfkVc56ggWDjZV bCpe438C/M9x+P4LNIAG0AAaQANoAA2gATRAq8cv3cD1IWG3PvYAAAAldEVYdGRhdGU6Y3JlYXRl ADIwMjItMTEtMThUMDM6MTU6NDUrMDE6MDCQX34SAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIyLTEx LTE4VDAzOjE1OjQ1KzAxOjAw4QLGrgAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vu PBoAAAAASUVORK5CYII="
+            ></image>   
+        </svg>
+        
+    )    
+}
