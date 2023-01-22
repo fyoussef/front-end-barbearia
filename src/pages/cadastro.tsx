3aa5b9487f0113a7ae653e4475b38447dc9ce05b
fyoussef <fillipyuou@gmail.com>
2023-01-22 19:29:28 -0300

    connect front with backend to register user

--- src/pages/cadastro.tsx
+++ src/pages/cadastro.tsx
@@ -1,8 +1,38 @@
+import axios from "axios";
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
 import { MustacheIcon } from "../components/Icons";
+import { useRouter } from "next/router";
 
 export default function SignUp() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  async function handleRegisterUser() {
+    if (!username || !email || !phone || !password) {
+      console.log("ERRO");
+      return;
+    }
+    const data = {
+      name: username,
+      email,
+      phone,
+      password,
+    };
+    const res = await axios.post("http://localhost:5000/user", data);
+    const { status } = res;
+
+    if (status == 201) {
+      console.log("REGISTRADO");
+      router.push("/login");
+    }
+  }
+
   return (
     <>
       <Head>
@@ -20,37 +50,41 @@ export default function SignUp() {
 
           <div>
             <p className="text-2xl font-semibold">Cadastro</p>
-            <p className="text-gray-500">Forneça os dados necessários para efetuar o seu cadastro</p>
+            <p className="text-gray-500">
+              Forneça os dados necessários para efetuar o seu cadastro
+            </p>
           </div>
 
           <div className="relative">
-              <input 
-                type="text"
-                name="floating_user" 
-                id="floating_user" 
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-                placeholder=" " 
-                required
-              />
-              <label 
-                htmlFor="floating_user" 
-                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Usuário
-              </label>
-          </div>
-
-          <div className="relative">
-            <input 
-              type="email"
-              name="floating_email" 
-              id="floating_email" 
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-              placeholder=" " 
+            <input
+              type="text"
+              name="floating_user"
+              id="floating_user"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
               required
+              onChange={(e) => setUsername(e.target.name)}
             />
-            <label 
-              htmlFor="floating_email" 
+            <label
+              htmlFor="floating_user"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Usuário
+            </label>
+          </div>
+
+          <div className="relative">
+            <input
+              type="email"
+              name="floating_email"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label
+              htmlFor="floating_email"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Email
@@ -58,16 +92,17 @@ export default function SignUp() {
           </div>
 
           <div className="relative">
-            <input 
+            <input
               type="tel"
-              name="floating_phone" 
-              id="floating_phone" 
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-              placeholder=" " 
+              name="floating_phone"
+              id="floating_phone"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
               required
+              onChange={(e) => setPhone(e.target.value)}
             />
-            <label 
-              htmlFor="floating_phone" 
+            <label
+              htmlFor="floating_phone"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Telefone
@@ -75,16 +110,17 @@ export default function SignUp() {
           </div>
 
           <div className="relative">
-            <input 
-              type="password" 
-              name="floating_password" 
-              id="floating_password" 
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-              placeholder=" " 
+            <input
+              type="password"
+              name="floating_password"
+              id="floating_password"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
               required
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <label 
-              htmlFor="floating_password" 
+            <label
+              htmlFor="floating_password"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Senha
@@ -92,20 +128,25 @@ export default function SignUp() {
           </div>
 
           <div>
-            <button 
-              type="button" 
-              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                Cadastre-se
+            <button
+              type="button"
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              onClick={handleRegisterUser}
+            >
+              Cadastre-se
             </button>
           </div>
 
           <div>
             <p className="text-sm text-center">
-              Já está cadastrado? <Link className="underline hover:text-blue-500" href='/login'>login</Link>
+              Já está cadastrado?{" "}
+              <Link className="underline hover:text-blue-500" href="/login">
+                login
+              </Link>
             </p>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
-import { format } from 'date-fns';
-import Head from 'next/head'
-import { useState } from 'react'
-import { Datepicker } from '../components/Datepicker'
-import { AvatarIcon } from '../components/Icons'
+import axios from "axios";
+import { format } from "date-fns";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { useState } from "react";
+import { Datepicker } from "../components/Datepicker";
+import { AvatarIcon } from "../components/Icons";
 
-export default function Home() {
+type HomeProps = {
+  schedules: {
+    hour: string;
+    barber_id: string;
+  }[];
+};
 
+export default function Home({ schedules }: HomeProps) {
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   function handleSelectedDate(selectedDate: Date) {
-    setSelectedDate(selectedDate)
+    setSelectedDate(selectedDate);
   }
 
   return (
@@ -21,38 +29,54 @@ export default function Home() {
       </Head>
 
       <div className="flex flex-col items-center mt-8">
-
         <Datepicker handleSelectedDate={handleSelectedDate} />
 
-        <div className="w-[546px] p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-[546px] p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700 mb-8">
           <div className="flex items-center justify-between mb-4">
-            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Agenda do dia {format(selectedDate, "dd/MM/yyyy")}</h5>
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+              Agenda do dia {format(selectedDate, "dd/MM/yyyy")}
+            </h5>
           </div>
           <div className="flow-root">
-            <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
-              <li className="py-3 sm:py-4 cursor-pointer">
-                <div className="flex items-center space-x-4">
-                  <div className="flex-shrink-0">
-                    <AvatarIcon />
+            <ul
+              role="list"
+              className="divide-y divide-gray-200 dark:divide-gray-700"
+            >
+              {schedules.map((schedule) => (
+                <li className="py-3 sm:py-4 cursor-pointer" key={schedule.hour}>
+                  <div className="flex items-center space-x-4">
+                    <div className="flex-shrink-0">
+                      <AvatarIcon />
+                    </div>
+                    <div className="flex-1 min-w-0">
+                      <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                        Horário Livre
+                      </p>
+                      <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                        {/* email@windster.com */}
+                      </p>
+                    </div>
+                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                      {schedule.hour}
+                    </div>
                   </div>
-                  <div className="flex-1 min-w-0">
-                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                      Neil Sims
-                    </p>
-                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                      email@windster.com
-                    </p>
-                  </div>
-                  <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                    09:00
-                  </div>
-                </div>
-              </li>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  /* const barber_id = "e32ef709-739a-4537-9f7f-3921e3f63900";
+
+  const res = await axios.get("http://localhost:5000/schedules/" + barber_id);
+  const { schedules } = res.data; */
+
+  return {
+    props: { schedules: [] },
+  };
+};
