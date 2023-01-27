318a506bf072bcafcad87cf546916e2be6240918
fyoussef <fillipyuou@gmail.com>
2023-01-26 21:19:06 -0300

    adjust set email state value

--- src/pages/cadastro.tsx
+++ src/pages/cadastro.tsx
@@ -101,7 +101,7 @@ export default function SignUp() {
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder=" "
               required
-              onChange={(e) => setEmail(e.target.name)}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <label
               htmlFor="floating_email"
